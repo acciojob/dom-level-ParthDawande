4ef6c0a143dc27af8afc6d5a098b5e6989d03ac9
ParthDawande <125895972+ParthDawande@users.noreply.github.com>
2025-11-14 15:50:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const element = document.getElementById("level");
+
+    let level = 0;
+    let current = element;
+
+    // Traverse upward until there is no parent
+    while (current) {
+        level++;
+        current = current.parentElement;
+    }
+
+    alert("The level of the element is: " + level);
